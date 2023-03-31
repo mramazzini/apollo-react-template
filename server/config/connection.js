dd7fc37c03197ed5ad32ca4813c4f7066bcc9d5c
Matteo Josue Ramazzini <90007656+mramazzini@users.noreply.github.com>
2023-03-31 18:47:09 -0500

    Update connection.js

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 mongoose
-  .connect(process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/edu-site", {
+  .connect(process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/mongo_database", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
